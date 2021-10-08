d11590752e23010e9da868f53f9195dd42feb7fe
choi <csw3411@gmail.com>
2021-10-08 10:30:50 +0900

    1~9 multiply

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int main(){
-	for(i=1;i<=8;++i)
-		for(j=1;j<=8;++j)
+	for(i=1;i<=9;++i)
+		for(j=1;j<=9;++j)
 			cout << i << " * " << j << " = " << i*j << '\n';
 	return 0;
 }
